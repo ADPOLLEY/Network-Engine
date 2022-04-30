6c89f7831714c679b1fdde2b42fa7f7c188a7015
Alex La Fazia <alex.j.lafazia@gmail.com>
2022-04-29 20:42:56 -0700

    Rendering FLASK HTML
    Added static and template folders for HTML and CSS files. This allow Flask to render the page. Added routes to direct users to webpage using /

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Network Engine!'
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/")
+def home():
+    """Serve homepage template."""
+    return render_template("index.html")
+
+@app.route("/inventory")
+def inventory():
+    """Serve inventory template."""
+    return render_template("inventory.html")
