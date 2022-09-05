73a4887a8225e645eadfcfa0620fd3daa1f1645f
Alex La Fazia <alex.j.lafazia@gmail.com>
2022-09-05 11:10:34 -0700

    Flask to Frontend
    Adding correct URL to href. Adding routes to each page.

--- app.py
+++ app.py
@@ -13,3 +13,28 @@ def home():
 def inventory():
     """Serve inventory template."""
     return render_template("inventory.html")
+
+@app.route("/config")
+def configy():
+    """Serve config template."""
+    return render_template("config.html")
+
+@app.route("/contact_us")
+def contact_us():
+    """Serve contact_us template."""
+    return render_template("contact_us.html")
+
+@app.route("/create_backup")
+def create_backup():
+    """Serve create_backup template."""
+    return render_template("create_backup.html")
+
+@app.route("/management")
+def management():
+    """Serve management template."""
+    return render_template("management.html")
+
+@app.route("/profile")
+def profile():
+    """Serve profile template."""
+    return render_template("profile.html")
