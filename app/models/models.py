a8f9b44e3b377ff72560f8995b5118118ec28874
Alex La Fazia <alex.j.lafazia@gmail.com>
2022-09-25 01:45:46 -0700

    UI cleanup, DB add
    Removed management page. Renamed backup config. Fix UI, add background color. Add model name to database

--- app/models/models.py
+++ app/models/models.py
@@ -3,5 +3,6 @@ from app import db
 class Device(db.Model):
     hostname_id = db.Column(db.String, primary_key=True, nullable=False, unique=False)
     subnet = db.Column(db.String, unique=False)
+    model = db.Column(db.String, unique=False)
     username = db.Column(db.String, unique=False)
     password = db.Column(db.String, unique=False)
